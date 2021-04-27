9c1d6903735158b88dcbed969e0c8d7735b16a78
alexellevu <newnapster83@gmail.com>
2021-04-27 16:10:43 +0200

    stampa nel log dei vari fizz buzz

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,15 @@
+var fiz = 'Fizz';
+var buz = 'Buzz';
+
 //stampare i numeri da 1 a 100
 for (var i = 1; i <= 100; i++){
-    console.log(i);
-}
+  
+  
+    if (i%3 == 0 ){ //selezionare i numeri che hanno modulo di 3 uguale a zero e stampare fizz
+    console.log(fiz);}
+      else if(i%5 == 0){ //selezionare i numeri che hanno modulo di 5 uguale a zero e stampare fizz
+        console.log(buz);}
+        else(i%3 == 0 && i%5 == 0);{ //selezionare i numeri che hanno sia modulo di 3 che   uguale a zero e stampare fizzbuzz
+            console.log(fiz + buz);
+        }
+    }
