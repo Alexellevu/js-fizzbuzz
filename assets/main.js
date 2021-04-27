31986c784ba076ad6b53a7c91151210091951858
alexellevu <newnapster83@gmail.com>
2021-04-27 16:36:33 +0200

    correzione condizioni

--- assets/main.js
+++ assets/main.js
@@ -5,20 +5,16 @@ var buz = 'Buzz';
 for (var i = 1; i <= 100; i++){
   
   
-    if (i%3 == 0 ){ //numeri che hanno modulo di 3 uguale a zero e stampare fizz
-    console.log(fiz);
-    
-    }
+    if (i%3 == 0 && i%5 == 0 ){ //numeri che hanno modulo di 3 uguale a zero e stampare fizz
+    console.log(fiz + buz); }
 
-      else if(i%5 == 0){ //numeri che hanno modulo di 5 uguale a zero e stampare fizz
-        console.log(buz);
-        
-    }
+      else if(i%3 == 0){ //numeri che hanno modulo di 5 uguale a zero e stampare fizz
+        console.log(fiz);}
             
-            else if(i%3 == 0 && i%5 == 0);{ //i numeri che hanno sia modulo di 3 che   uguale a zero e stampare fizzbuzz
-            console.log(fiz + buz);
-        }
-       /*  else{
+            else if(i%5 == 0){ //i numeri che hanno sia modulo di 3 che uguale a zero e stampare fizzbuzz
+            console.log(buz);
+            }
+     else{
         console.log(i);
-        } */
+        } 
 }
