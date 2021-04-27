2ddc605f492739b0469b592569835a1f090926b4
alexellevu <newnapster83@gmail.com>
2021-04-27 16:24:52 +0200

    else commentato

--- assets/main.js
+++ assets/main.js
@@ -5,11 +5,20 @@ var buz = 'Buzz';
 for (var i = 1; i <= 100; i++){
   
   
-    if (i%3 == 0 ){ //selezionare i numeri che hanno modulo di 3 uguale a zero e stampare fizz
-    console.log(fiz);}
-      else if(i%5 == 0){ //selezionare i numeri che hanno modulo di 5 uguale a zero e stampare fizz
-        console.log(buz);}
-        else(i%3 == 0 && i%5 == 0);{ //selezionare i numeri che hanno sia modulo di 3 che   uguale a zero e stampare fizzbuzz
+    if (i%3 == 0 ){ //numeri che hanno modulo di 3 uguale a zero e stampare fizz
+    console.log(fiz);
+    
+    }
+
+      else if(i%5 == 0){ //numeri che hanno modulo di 5 uguale a zero e stampare fizz
+        console.log(buz);
+        
+    }
+            
+            else if(i%3 == 0 && i%5 == 0);{ //i numeri che hanno sia modulo di 3 che   uguale a zero e stampare fizzbuzz
             console.log(fiz + buz);
         }
-    }
+       /*  else{
+        console.log(i);
+        } */
+}
